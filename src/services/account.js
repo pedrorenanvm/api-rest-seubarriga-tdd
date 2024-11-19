d8eb2199bc7459802c07ca3864b2f906c883ec26
Pedro Renan <pedroviana15094@gmail.com>
2024-11-19 15:29:12 -0300

    feat: add class 67

--- src/services/account.js
+++ src/services/account.js
@@ -28,6 +28,7 @@ module.exports = (app) => {
   const remove = async (id) => {
     const transaction = await app.services.transaction.findOne({ acc_id: id });
     if (transaction) throw new ValidationError('Essa conta possui transações associadas');
+    
     return app.db('accounts')
       .where({id})
       .del();
--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -7,7 +7,7 @@ const MAIN_ROUTE = '/v1/accounts';
 let user;
 let user2;
 
-beforeEach(async () => {
+beforeAll(async () => {
   const res = await app.services.user.save({ name: 'User Account', mail: `${Date.now()}@gmail.com`, password: '123456' })
   user = { ...res[0] };
   user.token = jwt.encode(user, 'Segredo!');
@@ -47,7 +47,10 @@ test('Não deve inserir uma conta de nome duplicado, para o mesmo usuário', () 
       });
 });
 
-test('Deve listar apenas as contas do usuário', () => { 
+test('Deve listar apenas as contas do usuário', async () => { 
+  await app.db('transactions').del();
+  await app.db('accounts').del();
+  
   return app.db('accounts').insert([
     {name: 'Acc User #1', user_id: user.id},
     {name: 'Acc User #2', user_id: user2.id},
