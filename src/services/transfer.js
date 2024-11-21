7c2f2e604fb585b36536bc64dc5c82a6d2d8d281
Pedro Renan <pedroviana15094@gmail.com>
2024-11-21 15:20:17 -0300

    feat: add classes 72, 73, 74

--- src/services/transfer.js
+++ src/services/transfer.js
@@ -1,3 +1,5 @@
+const ValidationError = require('../errors/ValidationError');
+
 module.exports = (app) => {
   const find = (filter = {}) => {
     return app.db('transfers')
@@ -5,7 +7,26 @@ module.exports = (app) => {
       .select();
   };
 
+  const findOne = (filter = {}) => {
+    return app.db('transfers')
+      .where(filter)
+      .first();
+  };
+
   const save = async (transfer) => {
+    if(!transfer.description) throw new ValidationError('Descrição é um atributo obrigatório');
+    if(!transfer.ammount) throw new ValidationError('Valor é um atributo obrigatório');
+    if(!transfer.date) throw new ValidationError('Data é um atributo obrigatório');
+    if(!transfer.acc_ori_id) throw new ValidationError('Conta de Origem é um atributo obrigatório');
+    if(!transfer.acc_dest_id) throw new ValidationError('Conta de Destino é um atributo obrigatório');
+    if(transfer.acc_ori_id === transfer.acc_dest_id) throw new ValidationError('Não é possível transferir de uma conta para ela mesma');
+    
+    const accounts = await app.db('accounts').whereIn('id', [transfer.acc_dest_id, transfer.acc_ori_id]);
+    accounts.forEach((acc) => {
+     if (acc.user_id !== parseInt(transfer.user_id, 10)) throw new ValidationError(`Conta #${acc.id} não pertence ao usuário`);
+    });
+
+
     const result = await app.db('transfers').insert(transfer, '*');
     const transferId = result[0].id;
 
@@ -18,5 +39,5 @@ module.exports = (app) => {
     return result;
   }
 
-  return { find, save };
+  return { find, save, findOne };
 }
--- test/routes/transfers.test.js
+++ test/routes/transfers.test.js
@@ -39,3 +39,80 @@ test('Deve inserir uma transferência comm sucesso', () => {
       expect(transactions[1].acc_id).toBe(10001);
     });
 });
+
+describe('Ao salvar uma transferencia válida', () => {
+  let transferId;
+  let income;
+  let outcome;
+
+  test('Deve retornar o status 201 e os dados da transferência', () => {
+    return request(app).post(MAIN_ROUTE)
+      .set('authorization', `bearer ${TOKEN}`)
+      .send({ description: 'Regular Transfer', user_id: 10000, acc_ori_id: 10000, acc_dest_id: 10001, ammount: 100, date: new Date() })
+      .then(async (res) => {
+
+        expect(res.status).toBe(201);
+        expect(res.body.description).toBe('Regular Transfer');
+        transferId = res.body.id;
+    });
+  });
+
+    test('As transações equivalentes devem ter sido geradas', async () => {
+      const transactions = await app.db('transactions').where({ transfer_id: transferId}).orderBy('ammount');
+      expect(transactions).toHaveLength(2);
+      [outcome, income] = transactions;
+    });
+
+    test('A transação de saída deve ser negativa', () => {
+      expect(outcome.description).toBe('Transfer to acc #10001');
+      expect(outcome.ammount).toBe('-100.00');
+      expect(outcome.acc_id).toBe(10000);
+      expect(outcome.type).toBe('O');
+    });
+
+  test('A transação de entrada deve ser positiva', () => {
+    expect(income.description).toBe('Transfer from acc #10000');
+    expect(income.ammount).toBe('100.00');
+    expect(income.acc_id).toBe(10001);
+    expect(income.type).toBe('I');
+  });
+
+  test('Ambas devem referenciar a trasnferência que a originou', () => {
+    expect(income.transfer_id).toBe(transferId);
+    expect(outcome.transfer_id).toBe(transferId);
+  });
+});
+
+describe('Ao tentar inserir uma transferência inválida...', () => {
+  const validTransfer = { description: 'Regular Transfer', user_id: 10000, acc_ori_id: 10000, acc_dest_id: 10001, ammount: 100, date: new Date() };
+
+  const template = (newData, errorMessage) => {
+    return request(app).post(MAIN_ROUTE)
+      .set('authorization', `bearer ${TOKEN}`)
+      .send({ ...validTransfer, ...newData })
+      .then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe(errorMessage);
+      });
+  };
+  
+  
+
+  test('Não deve inserir sem descrição', () => template({ description: null}, 'Descrição é um atributo obrigatório'));
+  test('Não deve inserir sem valor', () => template({ ammount: null }, 'Valor é um atributo obrigatório'));
+  test('Não deve inserir sem data', () => template({ date: null }, 'Data é um atributo obrigatório'));
+  test('Não deve inserir sem conta de origem', () => template({ acc_ori_id: null }, 'Conta de Origem é um atributo obrigatório'));
+  test('Não deve inserir sem conta de destino', () => template({ acc_dest_id: null }, 'Conta de Destino é um atributo obrigatório'));
+  test('Não deve inserir se as contas de origem e destino forem as mesmas', () => template({ acc_dest_id: 10000 }, 'Não é possível transferir de uma conta para ela mesma'));
+  test('Não deve inserir se as contas pertencerem a outro usuário', () => template({ acc_ori_id: 10002 }, 'Conta #10002 não pertence ao usuário')); 
+
+});
+
+test('Deve retornar uma transfêrencia por Id', () => {
+  return request(app).get(`${MAIN_ROUTE}/10000`)
+    .set('authorization', `bearer ${TOKEN}`)
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body.description).toBe('Transfer #1');
+    })
+});
